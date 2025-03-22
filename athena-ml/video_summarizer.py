457ab0ee4197406748beb73ffac0b55f8a5dacbf
simonlouis15 <simonlouis1932@gmail.com>
2025-03-21 23:30:05 -0400

    video summarizer using vertex ai

---
+++ athena-ml/video_summarizer.py
@@ -0,0 +1,18 @@
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+# TODO (developer): update project id
+PROJECT_ID = 'gen-lang-client-0178361373'
+vertexai.init(project=PROJECT_ID, location="us-central1")
+
+model = GenerativeModel("gemini-1.5-flash-002")
+
+contents = [
+    # Text prompt
+    "Summarize this video.",
+    #Youtube video of 3blue1brown
+    Part.from_uri("https://www.youtube.com/watch?v=aircAruvnKk&t=1s&ab_channel=3Blue1Brown", "video/mp4"),
+]
+
+response = model.generate_content(contents)
+print(response.text)
